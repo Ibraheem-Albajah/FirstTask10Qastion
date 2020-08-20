606b375324ec7cb2c571c212888c5813f7259c2a
Ibraheem-Albajah <68763482+Ibraheem-Albajah@users.noreply.github.com>
2020-08-20 13:25:19 +0300

    Update goodpairs.cs

--- Number of Good Pairs/goodpairs.cs
+++ Number of Good Pairs/goodpairs.cs
@@ -1,18 +1,10 @@
 public class Solution {
     public int NumIdenticalPairs(int[] nums) {
-            int i = 0 , j = 0 ,  pair=0 ; 
-        for(i=0 ; i<nums.Length;i++){
-            for(j=1; j<nums.Length;j++){
+            int  pair=0 ; 
+        for(int i=0 ; i<nums.Length;i++){
+            for(int j=1; j<nums.Length;j++){
                 if(nums[i]==nums[j]&&i<j)
                     pair+=1;
-                
-                
-            }
-            
-            
-            
-            
-            
         }
         return pair ;
     }
